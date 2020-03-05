e4646fad61fd67e1b23c42f5608232d59087d09c
Otto Kekäläinen <otto@seravo.fi>
2020-03-05 13:38:31 +0200

    Always use TRAVIS_COMMIT_RANGE if it 'git rev-list' returns commits

--- travis-env-debug.sh
+++ travis-env-debug.sh
@@ -42,3 +42,36 @@ Fails with:
 
 Commit d4a1fbc94074 refers to the old commit before it was amended, while
 eba29444d494 is the new commit that replaced the old one.
+
+Example of a push of a new branch:
+
+	TRAVIS_BRANCH: test-pr
+	TRAVIS_COMMIT: 66c96d0b399087f60ce66107caf6a8db117642be
+	TRAVIS_COMMIT_RANGE: 1c7e87c17181^...66c96d0b3990
+	TRAVIS_PULL_REQUEST_BRANCH:
+	TRAVIS_PULL_REQUEST_SHA:
+	TRAVIS_PULL_REQUEST_SLUG:
+	TRAVIS_REPO_SLUG: Seravo/gnitpick
+
+Example of a push on an existing branch:
+
+	TRAVIS_BRANCH: test-pr
+	TRAVIS_COMMIT: 90e7a7b1c94c8481f3f13bbbdceb7728fbd9ae2c
+	TRAVIS_COMMIT_RANGE: 66c96d0b3990...90e7a7b1c94c
+	TRAVIS_PULL_REQUEST_BRANCH:
+	TRAVIS_PULL_REQUEST_SHA:
+	TRAVIS_PULL_REQUEST_SLUG:
+	TRAVIS_REPO_SLUG: Seravo/gnitpick
+
+Example of a new pull request inside the same repository:
+
+	TRAVIS_BRANCH: master
+	TRAVIS_COMMIT: cd6793d3496c6d5bd5846819bcd1edf2d7df3d73
+	TRAVIS_COMMIT_RANGE: a10529ef030f1628e0fc4f981b63b530e6f3a6db...90e7a7b1c94c8481f3f13bbbdceb7728fbd9ae2c
+	TRAVIS_PULL_REQUEST_BRANCH: test-pr
+	TRAVIS_PULL_REQUEST_SHA: 90e7a7b1c94c8481f3f13bbbdceb7728fbd9ae2c
+	TRAVIS_PULL_REQUEST_SLUG: Seravo/gnitpick
+	TRAVIS_REPO_SLUG: Seravo/gnitpick
+
+The TRAVIS_COMMIT reflects the merge commit, if one was to be made. The
+TRAVIS_COMMIT_RANGE represents the actual range of commits in the PR.
